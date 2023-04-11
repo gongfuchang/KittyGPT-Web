d9e012bf936ad66f16a7e9d7959f59cb938b5605
alexgong <gronlet@126.com>
2023-04-11 21:11:15 +0800

    initial commit

---
+++ app/api/glm-stream/route.ts
@@ -0,0 +1,75 @@
+import { createParser } from "eventsource-parser";
+import { NextRequest } from "next/server";
+
+async function requestChatStream(req: NextRequest, url:string) {
+  
+  return fetch(url, {
+    headers: {
+      "Content-Type": "application/json"
+    },
+    method: req.method,
+    body: req.body,
+  });
+}
+async function createStream(req: NextRequest) {
+  const encoder = new TextEncoder();
+  const decoder = new TextDecoder();
+
+  const url = `http://127.0.0.1:7860/chat_stream`;
+  const res = await requestChatStream(req, url);
+
+
+  const contentType = res.headers.get("Content-Type") ?? "";
+  if (!contentType.includes("stream")) {
+    const content = await (
+      await res.text()
+    ).replace(/provided:.*. You/, "provided: ***. You");
+    console.log("[Stream] error ", content);
+    return "```json\n" + content + "```";
+  }
+
+  const stream = new ReadableStream({
+    async start(controller) {
+      function onParse(event: any) {
+        if (event.type === "event") {
+          const data = event.data;
+          // https://beta.openai.com/docs/api-reference/completions/create#completions/create-stream
+          if (data === "[DONE]") {
+            controller.close();
+            return;
+          }
+          try {
+            const json = JSON.parse(data);
+            const text = json.choices[0].delta.content;
+            const queue = encoder.encode(text);
+            controller.enqueue(queue);
+          } catch (e) {
+            controller.error(e);
+          }
+        }
+      }
+
+      const parser = createParser(onParse);
+      for await (const chunk of res.body as any) {
+        parser.feed(decoder.decode(chunk));
+      }
+    },
+  });
+  return stream;
+}
+
+export async function POST(req: NextRequest) {
+  try {
+    const stream = await createStream(req);
+    return new Response(stream);
+  } catch (error) {
+    console.error("[Chat Stream]", error);
+    return new Response(
+      ["```json\n", JSON.stringify(error, null, "  "), "\n```"].join(""),
+    );
+  }
+}
+
+export const config = {
+  runtime: "edge",
+};
---
+++ app/store/access.ts
@@ -0,0 +1,41 @@
+import { create } from "zustand";
+import { persist } from "zustand/middleware";
+import { queryMeta } from "../utils";
+
+export interface AccessControlStore {
+  accessCode: string;
+  token: string;
+
+  updateToken: (_: string) => void;
+  updateCode: (_: string) => void;
+  enabledAccessControl: () => boolean;
+  isAuthorized: () => boolean;
+}
+
+export const ACCESS_KEY = "access-control";
+
+export const useAccessStore = create<AccessControlStore>()(
+  persist(
+    (set, get) => ({
+      token: "",
+      accessCode: "",
+      enabledAccessControl() {
+        return queryMeta("access") === "enabled";
+      },
+      updateCode(code: string) {
+        set((state) => ({ accessCode: code }));
+      },
+      updateToken(token: string) {
+        set((state) => ({ token }));
+      },
+      isAuthorized() {
+        // has token or has code or disabled access control
+        return !!get().token || !!get().accessCode || !get().enabledAccessControl();
+      },
+    }),
+    {
+      name: ACCESS_KEY,
+      version: 1,
+    },
+  ),
+);
