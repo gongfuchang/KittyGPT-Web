011ee6e34d23dca68a4b5ee4376d945f28cdc585
alexgong <gronlet@126.com>
2023-04-15 11:28:49 +0800

    keep assistent to stream style of openai-stream

--- app/api/glm/request-utils.ts
+++ app/api/glm/request-utils.ts
@@ -19,36 +19,40 @@ export async function doCompletion(req: NextRequest) {
   const encoder = new TextEncoder();
   const decoder = new TextDecoder();
 
-  return requestGlm(req)
-  .then((response) => response.body)
-  .then((rb) => {
-    const reader = rb?.getReader();
-    const stream = new ReadableStream({
-      start(controller) {
-        // The following function handles each data chunk
-        function push() {
-          try {
-            // "done" is a Boolean and value a "Uint8Array"
-            reader?.read().then(({ done, value }) => {
-              // If there is no more data to read
-              if (done) {
-                controller.close();
-                return;
-              }
-              // Get the data and send it to the browser via the controller
-              const text = decoder.decode(value);
-              console.log('text-part:' + text);
-              controller.enqueue(value);
-              push();
-            });
-          } catch (error) {
-            controller.error(error);
-          }
+  const res = await requestGlm(req);
+  const rb = res.body as any;
+
+  // .then((response) => response.body)
+  // .then((rb) => {
+  //   console.log('process reder:')
+  //   const reader = rb?.getReader();
+  const stream = new ReadableStream({
+    start(controller) {
+      // The following function handles each data chunk
+      function push(reader: { read: () => Promise<{ done: any; value: any; }>; }) {
+        try {
+          // "done" is a Boolean and value a "Uint8Array"
+          reader.read().then(({ done, value }) => {
+            // If there is no more data to read
+            if (done) {
+              controller.close();
+              return;
+            }
+            // Get the data and send it to the browser via the controller
+            const text = decoder.decode(value);
+            console.log('text-part:' + text);
+            controller.enqueue(value);
+            // recursely invoke
+            push(reader);
+          });
+        } catch (error) {
+          controller.error(error);
         }
-        push();
-      },
-    });
-    return new Response(stream, { headers: { "Content-Type": "text/plain" } });
-  })
-  
+      }
+      const reader = rb?.getReader();
+      push(reader);
+    },
+  });
+  console.log('return response with 200');
+  return new Response(stream, { headers: { "Content-Type": "text/plain"} , status: 200 });
 }
